f2b66e819d103efec87b68b7ccce3ef256132a8a
Armando <armandooon@gmail.com>
2024-10-22 19:37:39 -0600

    Puse el codigo que mandaron en wha

--- src/routes/api/obtenerDatos/+server.js
+++ src/routes/api/obtenerDatos/+server.js
@@ -1,19 +1,6 @@
-import pkg from "pg"
-const { Pool } = pkg
-
-const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "postgres",
-    password: "naruto2010",
-    port: 5432
-})
-
 export async function GET() {
-    let client = await pool.connect()
-    let query = "SELECT * FROM tareas order by id"
-    let result = await client.query(query)
-    console.log(result.rows)
-
-    return new Response (JSON.stringify(result.rows), {status: 200})
+    let response = await fetch('http://13.58.249.92:8000/obtenerDatos')
+    let datos = await response.json()
+    console.log(datos)
+    return new Response(JSON.stringify(datos),{status: 200})
 }
